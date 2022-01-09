979a7b9f7d8cb48f477403f742e50318c9c69bb0
Rostyslav Zhabinskyi <zhabinskiirostislav@gmail.com>
2022-01-09 14:21:58 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { Switch } from 'react-router-dom';
 import Container from './components/Layout/Container';
 import AppBar from './components/Navigation/AppBar';
-import HomeView from './views/HomeView';
-import RegisterView from './views/RegisterView';
-import LoginView from './views/LoginView';
-import PrivatePageView from './views/PrivatePageView';
 import { authOperations } from './redux/auth';
 import { connect } from 'react-redux';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+
+const HomeView = lazy(() => import('./views/HomeView'));
+const RegisterView = lazy(() => import('./views/RegisterView'));
+const LoginView = lazy(() => import('./views/LoginView'));
+const PrivatePageView = lazy(() => import('./views/PrivatePageView'));
 
 class App extends Component {
   // рефреш
@@ -22,26 +23,28 @@ class App extends Component {
       <>
         <Container>
           <AppBar />
-          <Switch>
-            <PublicRoute exact path="/" component={HomeView} />
-            <PrivateRoute
-              path="/private"
-              redirectTo="/login"
-              component={PrivatePageView}
-            />
-            <PublicRoute
-              restricted
-              path="/register"
-              redirectTo="/private"
-              component={RegisterView}
-            />
-            <PublicRoute
-              restricted
-              path="/login"
-              redirectTo="/private"
-              component={LoginView}
-            />
-          </Switch>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Switch>
+              <PublicRoute exact path="/" component={HomeView} />
+              <PrivateRoute
+                path="/private"
+                redirectTo="/login"
+                component={PrivatePageView}
+              />
+              <PublicRoute
+                restricted
+                path="/register"
+                redirectTo="/private"
+                component={RegisterView}
+              />
+              <PublicRoute
+                restricted
+                path="/login"
+                redirectTo="/private"
+                component={LoginView}
+              />
+            </Switch>
+          </Suspense>
         </Container>
       </>
     );
